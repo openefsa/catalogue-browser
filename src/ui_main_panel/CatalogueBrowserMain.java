f407906c6d2f4257a4cdb49cda7755dd9027aa64
AVON Valentino <Valentino.AVON@ext.efsa.europa.eu>
2017-06-09 16:32:16 +0200

    Singleton Application
    A check to allow only one instance of the application at a time
    was added. If the application is started two times, the second time
    the user will get an error message.
    
    
    Signed-off-by: AVON Valentino <Valentino.AVON@ext.efsa.europa.eu>

--- src/ui_main_panel/CatalogueBrowserMain.java
+++ src/ui_main_panel/CatalogueBrowserMain.java
@@ -1,6 +1,10 @@
 package ui_main_panel;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
@@ -8,6 +12,7 @@ import org.eclipse.swt.widgets.Shell;
 
 import business_rules.WarningUtil;
 import catalogue_browser_dao.DatabaseManager;
+import messages.Messages;
 import utilities.GlobalUtil;
 import xml_reader.PropertiesReader;
 
@@ -16,13 +21,45 @@ public class CatalogueBrowserMain {
 	public static final String ApplicationName = PropertiesReader.getAppName();
 	public static final String ProgramVersion = PropertiesReader.getAppVersion();
 
+	private static ServerSocket socket;
+	private static final int PORT = 9999;  
+
+	/**
+	 * Close the application if it is already running 
+	 * in another instance
+	 */
+	private static void closeIfAlreadyRunning() {
+		
+		try {
+			//Bind to localhost adapter with a zero connection queue 
+			socket = new ServerSocket( PORT, 0, 
+					InetAddress.getByAddress( new byte[] {127,0,0,1} ) );
+		}
+		catch (BindException e) {
+			System.err.println( "Another instance of the catalogue browser is already running!" );
+			
+			GlobalUtil.showErrorDialog( new Shell(), 
+					Messages.getString( "AlreadyRunning.ErrorTitle" ),
+					Messages.getString( "AlreadyRunning.ErrorMessage" ) );
+
+			System.exit(1);
+		}
+		catch (IOException e) {
+			System.err.println( "Unexpected error." );
+			e.printStackTrace();
+			System.exit(2);
+		}
+	}
+	
 	/**
 	 * Main, catalogue browser entry point
 	 * 
 	 * @param args
 	 */
 	public static void main ( String[] args ) {
-
+		
+		closeIfAlreadyRunning();
+		
 		// application start-up message. Usage of System.err used for red chars
 		System.out.println( "Application Started " + System.currentTimeMillis() );
 
@@ -97,6 +134,14 @@ public class CatalogueBrowserMain {
 
 		// stop the database
 		DatabaseManager.stopMainDB();
+		
+		// close socket lock
+		try {
+			if ( socket != null )
+				socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		// exit the application
 		System.exit(0);
